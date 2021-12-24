dd9cd47d89442a9fcf16c6f0cfd22d039bf45540
kuro <93817520+EIJIKUROKAWA@users.noreply.github.com>
2021-12-24 22:38:05 +0900

    アクティブキャッシュの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,28 @@ validates :item_text, presence: true
 validates :item_category_id, presence: true
 validates :item_condition_id, presence: true
 validates :postage_included_id, presence: true
-validates :price, presence: true
+validates :price, presence: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
 validates :area_id, presence: true
 validates :send_day_id, presence: true
 validates :user, presence: true
 
 belongs_to :user  
 has_one_attached :image
+
+extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :item_quality
+  belomgs_to :okurisaki
+  belongs_to :hassou
+  belongs_to :haisouryou
+
+
+  validates :item_quality_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :haisouryou_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :okurisaki_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :hassou_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2021_12_24_083534) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "item_text", null: false
@@ -43,6 +49,11 @@ ActiveRecord::Schema.define(version: 2021_12_24_083534) do
     t.bigint "user_id", null: false
     t.integer "area_id", null: false
     t.integer "send_day_id", null: false
+    t.integer "okurisaki_id", null: false
+    t.integer "genre_id", null: false
+    t.integer "item_quality_id", null: false
+    t.integer "haisouryou_id", null: false
+    t.integer "hassou_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
