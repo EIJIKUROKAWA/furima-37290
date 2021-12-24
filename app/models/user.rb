6a4b41b2f1075786a2f74c808eea696ad416881e
kuro <93817520+EIJIKUROKAWA@users.noreply.github.com>
2021-12-24 11:41:32 +0900

    テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence:true
   validates :email, uniqueness: true
-  validates :encrypted_password, presence:true
-  validates :name_kanji_last, presence:true
-  validates :name_kanji_first, presence:true
-  validates :name_kana_last, presence:true
-  validates :name_kana_first, presence:true
+  validates :encrypted_password, presence:true, length: { minimum: 6 }
+  validates :name_kanji_last, presence:true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :name_kanji_first, presence:true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :name_kana_last, presence:true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :name_kana_first, presence:true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birth, presence:true        
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,16 +4,24 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
+ 
 
 
-  describe '新規登録/ユーザー情報' do
+  describe 'ユーザー新規登録' do
+     context '新規登録できるとき' do
+      it "全ての値が入っていたら登録できる。" do
+      expect(@user).to be_valid
+      end
+     end
+   
+     context '新規登録できないとき' do
     it 'ニックネームが空の場合' do
-      @user.nickname = ' '
+      @user.nickname = ''
       @user.valid?
       expect(@user.errors.full_messages).to include ("Nickname can't be blank")
     end
     it 'メールアドレスが空の場合' do
-      @user.email = ' '
+      @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include ("Email can't be blank")
       
@@ -21,51 +29,88 @@ RSpec.describe User, type: :model do
   
 
     it 'パスワードが空の場合' do
-      @user.password = ' '
+      @user.password = ''
       @user.valid?
       expect(@user.errors.full_messages).to include ("Password can't be blank")
     end
 
 
     it 'パスワードとパスワード（確認）は、値の一致が必須であること' do
-      @user.password_confirmation = ' '
+      @user.password_confirmation = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
   
 
     it 'お名前、苗字(漢字）が空の場合' do
-      @user.name_kanji_last = ' ' 
+      @user.name_kanji_last = '' 
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last_name_kanji can't be blank")
+      expect(@user.errors.full_messages).to include("Name kanji last can't be blank")
       
     end
 
     it 'お名前、名前(漢字）が空の場合' do
-      @user.name_kanji_first = ' '
+      @user.name_kanji_first = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("First_name_kanji can't be blank")
+      expect(@user.errors.full_messages).to include("Name kanji first can't be blank")
     end
 
     it 'お名前、苗字(カナ) が空の場合' do
-      @user.name_kanji_last = ' '
+      @user.name_kana_last = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last_name_kana can't be blank")
+      expect(@user.errors.full_messages).to include("Name kana last can't be blank")
     end
 
-    it '生年月日が必須であること。' do
-      @user.birth = " "
+    it 'お名前、名前(カナ) が空の場合' do
+      @user.name_kana_first = ''
       @user.valid?
-      except(@user.errors.full_messages).to include("Birth can't be blank")
+      expect(@user.errors.full_messages).to include("Name kana first can't be blank")
     end
 
+    it 'お名前、名前(カナ) が全角カナじゃない場合' do
+      @user.name_kana_last = 'test'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name kana last is invalid")
+    end
+
+    it 'お名前、苗字(カナ) が全角カナじゃない場合' do
+      @user.name_kana_first = 'test'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name kana first is invalid")
+    end
+
+
+
+    it '生年月日が空の場合。' do
+      @user.birth = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth can't be blank")
+    end
+
+    it 'passwordが5文字以下であれば登録できない' do
+      @user.password = '1234i'
+      @user.password_confirmation = '1234i'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
     
+    it 'お名前、苗字(全角) が全角じゃない場合' do
+      @user.name_kanji_last = 'test'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name kanji last is invalid")
+    end
 
+    it 'お名前、名前(全角) が全角じゃない場合' do
+      @user.name_kanji_first = 'test'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name kanji first is invalid")
+    end
+
+   end
   end
-
+end
   
 
  
 
 
-end
