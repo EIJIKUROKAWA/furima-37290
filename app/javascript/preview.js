579461da350aa307b45fa48432f29aa5da3073d7
kuro <93817520+EIJIKUROKAWA@users.noreply.github.com>
2022-01-06 21:16:12 +0900

    商品のプレビュー実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // 新規投稿・編集ページのフォームを取得
+  const itemForm = document.getElementById('new_item');
+  const previewList = document.getElementById('previews');
+  if (!itemForm) return null;
+  console.log("preview.jsが読み込まれました");
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    // 古いプレビューが存在する場合は削除
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    console.log("input要素で値の変化が起きました");
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    // 表示する画像を生成
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
+});
